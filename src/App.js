556f502d06baa8fd66bfa40869006df43f46bb10
Dung Phan <dungphan@Dungs-MacBook-Pro.local>
2019-04-22 21:06:52 +0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import Header from "./components/common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./components/courses/CoursesPage";
+
+export default function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
